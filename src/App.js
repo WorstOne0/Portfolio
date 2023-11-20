739b156b7a811bfb7b10121b4fbbbb73eb4ab4c4
WorstOne0 <luccagabriel12@hotmail.com>
2023-11-20 01:27:11 -0300

    UP

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import MyRoutes from "./routes";
-
-import "./assets/styles/global.css";
-
-function App() {
-  return <MyRoutes />;
-}
-
-export default App;
--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-import NavBar from "./NavBar";
-import Layout from "./Layout";
-import AnimatedLetters from "./AnimatedLetters";
-import MyParticles from "./MyParticles";
-
-export { NavBar, Layout, AnimatedLetters, MyParticles };
